d2230570158d8fee06cf1c6fb9b33e9af70ab6d6
Pratik Chakraborty <pratik@talkinglands.com>
2022-08-01 19:24:01 +0530

    working-state

--- src/Map.js
+++ src/Map.js
@@ -6,10 +6,10 @@ import { useMemo } from "react";
 import { useState } from "react";
 import { useCallback } from "react";
 import { useRef } from "react";
-import 'leaflet/dist/leaflet.css';
+// import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import Sidenav from './Sidenav'
-
+// Starting point of map
 const center = {
     lat: 51.505,
     lng: -0.09,
@@ -17,7 +17,7 @@ const center = {
 
   // const newLat = marker.getLatLng().lat;
   // const newLng = marker.getLatLng().lng;
-
+// Dragging and getting lat lng start
 function Map() {
     const [position, setPosition] = useState(center)
     const markerRef = useRef(null)
@@ -25,6 +25,7 @@ function Map() {
     const [lngpoints,setlngpoints] = useState('')
 
     const eventHandlers = useMemo(() => ({
+      //function to get lat-lng after setting point
       dragend() {
         const marker = markerRef.current
         setlatpoints(marker.getLatLng().lat)
@@ -35,8 +36,12 @@ function Map() {
     )
     const draggable=true; 
     return (
-        <div id="map">
-        <Sidenav latpoints={latpoints} lngpoints={lngpoints}/>
+        <div id="map" className="map-view">
+          <div>
+          <Sidenav latpoints={latpoints} lngpoints={lngpoints}/>  {/*Passing lat-lng to the form*/}
+          </div>
+        {/* <Sidenav latpoints={latpoints} lngpoints={lngpoints}/> */}
+        <div className="map-container">
         <MapContainer center={[51.505, -0.09]} zoom={18} scrollWheelZoom={false} >
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -49,6 +54,19 @@ function Map() {
             ref={markerRef}></Marker>
         
         </MapContainer>
+        </div>
+        {/* <MapContainer center={[51.505, -0.09]} zoom={18} scrollWheelZoom={false} >
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={center}
+            draggable={draggable}
+            eventHandlers={eventHandlers}
+            //position={position}
+            ref={markerRef}></Marker>
+        
+        </MapContainer> */}
         </div>
     );
 }
--- src/Sidenav.js
+++ src/Sidenav.js
@@ -6,11 +6,13 @@ import { MapContainer, TileLayer,Marker,Popup,useMapEvents,Circle,CircleMarker,P
 // import { marker } from "leaflet";
 import { useMemo } from "react";
 import { useRef } from "react";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
 
 function Sidenav(props) {
-    console.log(props)
+     //console.log(props)
 
-    const url = ""
+    const url = "" 
     const [data, setData] = useState({
         latitude: "",
         longitude: "",
@@ -18,7 +20,7 @@ function Sidenav(props) {
     })
 
 
-    function submit(e) {
+    /*function submit(e) {
         e.preventDefault();
         Axios.post(url, {
             latitude: data.latitude,
@@ -28,38 +30,81 @@ function Sidenav(props) {
         .then(res=>{
             console.log(res.data)
         })
-    }
+    }*/
 
+    // url = https://jsonbase-8e899-default-rtdb.firebaseio.com
+
+    /*const [details, setDetails] = useState({
+        lat: '',
+        long: '',
+        story: '',
+       
+    })*/
+
+    /*const postData = async(e) => {
+        e.preventDefault()
+        const{lat, long, story} = details;
+        const res = await fetch("https://jsonbase-8e899-default-rtdb.firebaseio.com/latlng.json",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                lat,
+                long,
+                story,
+            })
+        })
+    }*/
+
+
+    // Handle function for seting the data in the form
+    //LatLng are comming from props from the Map.js component
     function handle(e) {
         const newData = e.target.value;
-        setData({
-            ...data,
-            [e.target.name]:newData
-        })
+        
+        setData(()=>({
+            latitude:props.latpoints,
+            longitude:props.lngpoints,
+            story:e.target.value
+        }))
+        // ...data,
+        // [e.target.name]:newData
 
         // const newData = {...data}
         // newData[e.target.id] = e.target.value
         // setData(newData)
-        console.log(data)
+       
     }
-
+    // Handle click event, 
+    // this fun will be called after the btn-click and will upload in the database
+    // Initialising the onClick function to push with async
+    const handleOnCLick = async(e)=>{
+        e.preventDefault();
+        console.log(data)
+        axios.post('https://jsonbase-8e899-default-rtdb.firebaseio.com/sample.json',data)
+        .then(response =>console.log("response",response))
+        .catch(json => console.log(json))
+    }
+//form start
     return (
         <div className='sidebar'>
             <div className='title'>
                 <h1>Talking Lands</h1>
             </div>
-            <form class="story-form" onSubmit={(e)=>submit(e)}>
+            <form className="story-form">
                 <input onChange={(e)=>handle(e)} type="text" id="latitude"   value={props.latpoints} placeholder="Latitude" name="latitude"/>
-                <input onChange={(e)=>handle(e)} type="text" id="longitude" value={props.lngpoints} placeholder="Longitude" name="longitude"/>
+                <input onChange={(e) =>handle(e)} type="text" id="longitude" value={props.lngpoints} placeholder="Longitude" name="longitude"/>
                 <input onChange={(e)=>handle(e)} type="textarea" id="story" value={data.story} name="story" placeholder="Enter your story" style={{"height": 80}}/>
-                <div class="drag-area">
+                <div className="drag-area">
                     <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
                     <header>Drag and Drop to Upload File</header>
                     <span>OR</span>
-                    <button>Browse File</button>
+                    <button>Browse File</button> {/*Image upload feature left*/}
                     <input type="file" hidden/>
                 </div>
-                <button type="click" id="submit-btn">Enter Story</button>
+                <button onClick={handleOnCLick} type="click" id="submit-btn">Enter Story</button>
+                
             </form>
         </div>
     );
