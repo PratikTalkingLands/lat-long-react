6d7259d4ccf40714129b9f993dfc7291b4af5b9b
Pratik Chakraborty <pratik@talkinglands.com>
2022-08-02 13:38:55 +0530

    image to json

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css'
-import { MapContainer, TileLayer,Marker,Popup,useMapEvents,Circle,CircleMarker,Polyline,ScaleControl} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Circle, CircleMarker, Polyline, ScaleControl } from 'react-leaflet';
 import { map } from "leaflet";
 import { useMemo } from "react";
 import { useState } from "react";
@@ -11,51 +11,52 @@ import L from 'leaflet';
 import Sidenav from './Sidenav'
 // Starting point of map
 const center = {
-    lat: 51.505,
-    lng: -0.09,
-  }
+  lat: 51.505,
+  lng: -0.09,
+}
 
-  // const newLat = marker.getLatLng().lat;
-  // const newLng = marker.getLatLng().lng;
+// const newLat = marker.getLatLng().lat;
+// const newLng = marker.getLatLng().lng;
 // Dragging and getting lat lng start
 function Map() {
-    const [position, setPosition] = useState(center)
-    const markerRef = useRef(null)
-    const [latpoints,setlatpoints] = useState('')
-    const [lngpoints,setlngpoints] = useState('')
+  const [position, setPosition] = useState(center)
+  const markerRef = useRef(null)
+  const [latpoints, setlatpoints] = useState('')
+  const [lngpoints, setlngpoints] = useState('')
 
-    const eventHandlers = useMemo(() => ({
-      //function to get lat-lng after setting point
-      dragend() {
-        const marker = markerRef.current
-        setlatpoints(marker.getLatLng().lat)
-        setlngpoints(marker.getLatLng().lng)
-      },
-    }),
+  const eventHandlers = useMemo(() => ({
+    //function to get lat-lng after setting point
+    dragend() {
+      const marker = markerRef.current
+      setlatpoints(marker.getLatLng().lat)
+      setlngpoints(marker.getLatLng().lng)
+      console.log(marker.getBounds())
+    },
+  }),
     [],
-    )
-    const draggable=true; 
-    return (
-        <div id="map" className="map-view">
-          <div>
-          <Sidenav latpoints={latpoints} lngpoints={lngpoints}/>  {/*Passing lat-lng to the form*/}
-          </div>
-        {/* <Sidenav latpoints={latpoints} lngpoints={lngpoints}/> */}
-        <div className="map-container">
+  )
+  const draggable = true;
+  return (
+    <div id="map" className="map-view">
+      <div>
+        <Sidenav latpoints={latpoints} lngpoints={lngpoints} />  {/*Passing lat-lng to the form*/}
+      </div>
+      {/* <Sidenav latpoints={latpoints} lngpoints={lngpoints}/> */}
+      <div className="map-container">
         <MapContainer center={[51.505, -0.09]} zoom={18} scrollWheelZoom={false} >
-        <TileLayer
+          <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={center}
+          />
+          <Marker position={center}
             draggable={draggable}
             eventHandlers={eventHandlers}
             //position={position}
             ref={markerRef}></Marker>
-        
+
         </MapContainer>
-        </div>
-        {/* <MapContainer center={[51.505, -0.09]} zoom={18} scrollWheelZoom={false} >
+      </div>
+      {/* <MapContainer center={[51.505, -0.09]} zoom={18} scrollWheelZoom={false} >
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -67,8 +68,8 @@ function Map() {
             ref={markerRef}></Marker>
         
         </MapContainer> */}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Map;
--- src/Sidenav.js
+++ src/Sidenav.js
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Axios from 'axios'
 import './App.css'
 import Map from './Map';
-import { MapContainer, TileLayer,Marker,Popup,useMapEvents,Circle,CircleMarker,Polyline,ScaleControl} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Circle, CircleMarker, Polyline, ScaleControl } from 'react-leaflet';
 // import { marker } from "leaflet";
 import { useMemo } from "react";
-import { useRef } from "react";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
 
-function Sidenav(props) {
-     //console.log(props)
 
-    const url = "" 
+function Sidenav(props) {
+    //console.log(props)
+
+
+    const fileInputRef = useRef(null)
+
+
+
+    const url = ""
     const [data, setData] = useState({
         latitude: "",
         longitude: "",
-        story: ""
+        story: "",
+        image:""
     })
+
+    // const [image, setImage] = useState()
 
 
     /*function submit(e) {
@@ -62,49 +70,84 @@ function Sidenav(props) {
     //LatLng are comming from props from the Map.js component
     function handle(e) {
         const newData = e.target.value;
-        
-        setData(()=>({
-            latitude:props.latpoints,
-            longitude:props.lngpoints,
-            story:e.target.value
+
+        setData(() => ({
+            latitude: props.latpoints,
+            longitude: props.lngpoints,
+            story: e.target.value,
+            
         }))
+        // setData({
+        //     ...data,
+        //     [e.target.name]:e.target.value
+        // })
         // ...data,
         // [e.target.name]:newData
 
         // const newData = {...data}
         // newData[e.target.id] = e.target.value
         // setData(newData)
-       
+
     }
     // Handle click event, 
     // this fun will be called after the btn-click and will upload in the database
     // Initialising the onClick function to push with async
-    const handleOnCLick = async(e)=>{
+    const handleOnCLick = async (e) => {
         e.preventDefault();
         console.log(data)
-        axios.post('https://jsonbase-8e899-default-rtdb.firebaseio.com/sample.json',data)
-        .then(response =>console.log("response",response))
-        .catch(json => console.log(json))
+        axios.post('https://jsonbase-8e899-default-rtdb.firebaseio.com/sample.json', data)
+            .then(response => console.log("response", response))
+            .catch(json => console.log(json))
     }
-//form start
+
+    const handleImage = (e) => {
+        // hiddenBtn.current.click()
+        // console.log(e.target.files[0])
+        e.preventDefault()
+        setData(()=>({
+            ...data,
+            image:URL.createObjectURL(e.target.files[0])
+        }))
+    }
+    console.log(data.image)
+
+    /*const handleFileChange = (e) => {
+        e.preventDefault()
+        let a = e.target.files[0]
+        //setImage(a);
+    }*/
+    //form start
     return (
         <div className='sidebar'>
             <div className='title'>
                 <h1>Talking Lands</h1>
             </div>
             <form className="story-form">
-                <input onChange={(e)=>handle(e)} type="text" id="latitude"   value={props.latpoints} placeholder="Latitude" name="latitude"/>
-                <input onChange={(e) =>handle(e)} type="text" id="longitude" value={props.lngpoints} placeholder="Longitude" name="longitude"/>
-                <input onChange={(e)=>handle(e)} type="textarea" id="story" value={data.story} name="story" placeholder="Enter your story" style={{"height": 80}}/>
-                <div className="drag-area">
-                    <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
-                    <header>Drag and Drop to Upload File</header>
-                    <span>OR</span>
-                    <button>Browse File</button> {/*Image upload feature left*/}
-                    <input type="file" hidden/>
-                </div>
+                <input onChange={(e) => handle(e)} type="text" id="latitude" value={props.latpoints} placeholder="Latitude" name="latitude" />
+                <input onChange={(e) => handle(e)} type="text" id="longitude" value={props.lngpoints} placeholder="Longitude" name="longitude" />
+                <input onChange={(e) => handle(e)} type="textarea" id="story" value={data.story} name="story" placeholder="Enter your story" style={{ "height": 80 }} />
+
+                {/* 
+                 */}
+                <input
+                    type="file"
+                    multiple={true}
+                    ref={fileInputRef}
+                    name="image"
+                    onChange={handleImage}
+                    
+                />
+
+                <button
+                    className='user-change-btn'
+                    onClick={() => fileInputRef.current.click()}
+                    hidden
+                    >
+                    Change profile
+                </button>
+
                 <button onClick={handleOnCLick} type="click" id="submit-btn">Enter Story</button>
-                
+
             </form>
         </div>
     );
